13fe617d025ce1e4b5432fada744213db6fc1525
osiri <osiri@LAPTOP-MTZ>
2020-10-31 22:03:03 -0600

    Se modificó el constructor y se agregaron los métodos ImprimeAlumno ImprimeClavesAsignaturas y addClaveAsignaturas

--- Proyecto Colecciones en Java/src/proyecto/colecciones/en/java/Alumno.java
+++ Proyecto Colecciones en Java/src/proyecto/colecciones/en/java/Alumno.java
@@ -3,7 +3,15 @@ package proyecto.colecciones.en.java;
 public class Alumno {
     private String Nombre;
     private long numeroCuenta;
+    private int gruposInscritos = 0;
     private String[] clavesGrupos = new String[3];
+    private static int numAlumnos=0;
+    
+    public Alumno (String Nombre, long numeroCuenta){
+        numAlumnos++;
+        setNombre(Nombre);
+        setNumeroCuenta(numeroCuenta);
+    }
     
     public String getNombre() {
         return Nombre;
@@ -25,7 +33,30 @@ public class Alumno {
         return clavesGrupos;
     }
     
-    public void setClavesGrupos(String [] clavesGrupos) { 
-        this.clavesGrupos = clavesGrupos;
+    public void setClavesGrupos(String claveGrupo1, String claveGrupo2, String claveGrupo3) { 
+        this.clavesGrupos[0] = claveGrupo1;
+        this.clavesGrupos[1] = claveGrupo2;
+        this.clavesGrupos[2] = claveGrupo3;
+    }
+    
+    public void imprimeAlumno(){
+        System.out.println("Nombre del Alumno: "+getNombre());
+        System.out.println("Número de Cuenta: "+getNumeroCuenta());
+    }
+    
+    public void imprimeClavesGrupos(){
+        if(clavesGrupos == null || clavesGrupos.length == 0){  
+            System.out.println("No está inscrito a ninguna Asigantura");
+        } else { 
+            System.out.println("Las Claves de las Asignaturas a las que está inscrito: ");
+            for (String i: clavesGrupos) {
+                System.out.println(" - "+i);
+            }
+        }
+    }
+    
+    public void addClaveGrupos(String ClaveGrupo){
+        this.clavesGrupos[gruposInscritos] = ClaveGrupo;
+        gruposInscritos++;
     }
 }
