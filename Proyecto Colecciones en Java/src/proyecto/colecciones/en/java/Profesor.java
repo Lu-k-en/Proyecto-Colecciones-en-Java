c6767895183d282e052c57b5a35d831a2543e459
andy <andy@192.168.1.73>
2020-11-02 03:37:35 -0600

    Regresando a la normalidad v3

--- Proyecto Colecciones en Java/src/proyecto/colecciones/en/java/Profesor.java
+++ Proyecto Colecciones en Java/src/proyecto/colecciones/en/java/Profesor.java
@@ -6,10 +6,7 @@ import java.util.List;
 public class Profesor {
     private String gradoAcademico;
     private String nombre;
-    private String correo;
-    
-    // private int gruposInscritos = 0; // Podemos saber el número de grupos inscritos según el tamaño de la lista clavesGrupos
-    
+    private String correo; 
     private List<String> claveGrupos = new LinkedList<>();
 
     
@@ -19,55 +16,37 @@ public class Profesor {
         setCorreo(correo);
         
     }
-   
-    // Modificar para agregar a la lista 
-    /*public void setClaveGrupos(String claveGrupo1, String claveGrupo2, String claveGrupo3, String claveGrupo4, String claveGrupo5) {
-        this.claveGrupos[0] = claveGrupo1;
-        this.claveGrupos[1] = claveGrupo2;
-        this.claveGrupos[2] = claveGrupo3;
-        this.claveGrupos[3] = claveGrupo4;
-        this.claveGrupos[4] = claveGrupo5;
-    }*/
     
     public void imprimeProfesor(){
         System.out.println("\tNombre del Profesor: "+nombre);
         System.out.println("\tGrado Académico: "+gradoAcademico);
         System.out.println("\tCorreo de contacto: "+correo);
     }
-    
-    
-    // Modificar para imprimir las claves de la lista
-    public void imprimeClaveGrupos(){
-        if(claveGrupos == null || claveGrupos.size() == 0){  
-            System.out.println("No imparte ninguna asignatura");
-        
-        } else { 
-            System.out.println("Las Claves de las Asignaturas que imparte: ");
-            for (String i: claveGrupos) {
-                System.out.println(" - "+i);
-            }
-        }
-    }
-    
-    //Modificar para agregar a lista, recibe un long numCuenta
-    public void addClaveGrupo(String ClaveGrupo){
-        this.claveGrupos.add(ClaveGrupo);
-    }
-           
+      
     public String getGradoAcademico() {
         return gradoAcademico;
     }
     
     public void setGradoAcademico(String gradoAcademico) {
-        this.gradoAcademico = gradoAcademico;
+        if(gradoAcademico.length() > 1 && gradoAcademico.length() < 4){
+            this.gradoAcademico = gradoAcademico;
+        }else{
+            System.out.println("El grado académico debe de contener de 2 a 3 letras Ej. MI, I, DR");
+            this.gradoAcademico = null;
+        }    
     }
     
     public String getNombre() {
         return nombre;
     }
     
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setNombre(String nombre){
+        if(nombre.length() > 2 && nombre.length() < 50){
+            this.nombre = nombre;
+        }else{
+            System.out.println("El nombre debe tener al menos 3 caracteres y menos de 50");
+            this.nombre = null;
+        }            
     }
     
     public String getCorreo() {
@@ -75,8 +54,31 @@ public class Profesor {
     }
     
     public void setCorreo(String correo) {
-        this.correo = correo;
+        if(correo.contains("@")){
+            this.correo = correo;
+        }else{
+            System.out.println("El correo debe tener un @");
+            this.correo = null;
+        }
+    }
     
+    
+   
+    public void imprimeClaveGrupos() {
+        if (claveGrupos == null || claveGrupos.size() == 0) {
+            System.out.println("No imparte ninguna asignatura");
+
+        } else {
+            System.out.println("Las Claves de las Asignaturas que imparte: ");
+            for (String i : claveGrupos) {
+                System.out.println(" - " + i);
+            }
+        }
+    }
+    
+    //Modificar para agregar a lista, recibe un long numCuenta
+    public void addClaveGrupo(String ClaveGrupo) {
+        this.claveGrupos.add(ClaveGrupo);
     }
     
 }
--- Proyecto Colecciones en Java/src/proyecto/colecciones/en/java/ProyectoColeccionesEnJava.java
+++ Proyecto Colecciones en Java/src/proyecto/colecciones/en/java/ProyectoColeccionesEnJava.java
@@ -10,68 +10,70 @@ public class ProyectoColeccionesEnJava {
         
         //Hashtable key = #cuenta, value = alumno || clave, asignatura
         HashMap<Long,Alumno> alumnos = new HashMap<>();
-        HashMap<Long,Profesor> profesores = new HashMap<>();
-        HashMap<Integer,Asignatura> asignaturas = new HashMap<>();
-        
-        //Hashtable<Integer, Asignatura> asignaturas = new Hashtable<>();
-        //Set<Long> numerosCuenta = new HashSet<>();
-        
-        //List<Asignatura> asignaturas = new LinkedList<>();
-        //Set<Integer> clavesAsignaturas = new HashSet<>();
-        
+        //HashMap<Long,Profesor> profesores = new HashMap<>();
+        HashMap<Integer,Asignatura> asignaturas = new HashMap<>(); 
         HashMap<String, Grupo> grupos = new HashMap<>();
 
         Scanner sc = new Scanner(System.in);
         
-        int op;
+        int selector = 0;
         String nombre;
         int clave;
         int opAux;
         int opAux2;
         //
         do{
-            System.out.println("###  Menu Principal  ###");
-            
-            System.out.println("Actualmente cuento con: ");
-            System.out.println("\t " + Alumno.getNumAlumnos()+ " alumnos");
-            System.out.println("\t " + Profesor.getNumProfesores() + " profesores\n");
-            String[] opciones = {"Ver", "Crear", "edi"};
-            
-            /*
-            System.out.println("\t1.Crear alumno");
-            System.out.println("\t2.Crear profesor");
-            System.out.println("\t3.Crear asignatura");
-            System.out.println("\t4.Crear Grupo");
-            System.out.println("\t5.Mostrar Alumnos");
-            System.out.println("\t6.Mostrar Profesores");
-            System.out.println("\t7.Mostrar Asignaturas");
-            System.out.println("\t8.Mostrar Grupos");
-            System.out.println("\t99.Salir");
-            */
-            op = Menu.menu(opciones);
-            
-            switch(op){
+           
+          
+            switch(selector){
                
+                case 0->{
+                
+                    System.out.println("###  Menu Principal  ###");
+
+                    System.out.println("Actualmente cuento con: ");
+                    System.out.println("\t " + alumnos.size() + " alumnos");
+                    //System.out.println("\t " + profesores.size() + " profesores");
+
+                    System.out.println("\nIndicame por favor, sobre que entidad deseas trabajar?");
+                    
+                    String[] opciones = {"Alumnos", "Profesores", "Grupos", "Asignaturas", "Sácame de aqui"};
+                    selector = Menu.menu(opciones);
+                    
+                }
+                
                 case 1->{
                     
-                    System.out.println("\n ** Alumnos ");
-                    String[] subOpciones = {"Crear alumno", "Mostrar alumno"};
+                    System.out.println("\n** Alumnos ");
+                    String[] subOpciones = {"Crear alumno", "Mostrar alumno", "Modificar Alumno", "Volver"};
                     
-                    int ops = Menu.menu(subOpciones);
-                    
-                    System.out  .println("Ingrese el número de cuenta del alumno");
-                    long numCuenta = Long.valueOf(sc.next()); 
-                    if (alumnos.containsKey(numCuenta)){
-                        System.out.println("Ya existe este alumno");
-                    }else{
-                        System.out.println("Ingrese el nombre del alumno");
-                        nombre = sc.next();
-                        alumnos.put(numCuenta, new Alumno(nombre));
-                                               
+                    int subSelector = Menu.menu(subOpciones);
+                        
+                    switch(subSelector){
+                        case 1 ->{
+                            System.out.println("\n* Creando Alumno");
+                            System.out.println("Ingrese los siguientes datos");
+                            System.out.print("\nNúmero de cuenta: ");
+                            long numCuenta = sc.nextLong();
+                            sc.nextLine();
+                            
+                            if(alumnos.containsKey(numCuenta)){
+                                System.out.println("Lo siento, ya existe un alumno con ese número de cuenta");
+                            }else{
+                                System.out.print("Nombre completo: ");
+                                nombre = sc.nextLine();
+                                alumnos.put(numCuenta, new Alumno(nombre));
+                            }
+                            
+                            break;
+                        }
                     }
+                        
                     break;
                 }
-                case 2->{
+                
+                default ->{ System.out.println("Opción no válida"); break;}
+                /*case 2->{
                     System.out.println("Ingrese el número de cuenta del profesor");
                     long numCuenta = sc.nextLong();
                     if (profesores.containsKey(numCuenta)){
@@ -87,9 +89,9 @@ public class ProyectoColeccionesEnJava {
                         String correo = sc.next();     
                         profesores.put(numCuenta, new Profesor(nombre, gradoProfesor, correo));
                     
-                    }
+                    }*/
                 }
-                case 3 -> {
+                /*case 3 -> {
                     System.out.println("Ingrese la clave de la asignatura");
                     clave = sc.nextInt();
                     if (asignaturas.containsKey(clave)){
@@ -105,10 +107,10 @@ public class ProyectoColeccionesEnJava {
                         int semestre = sc.nextInt();
                         
                         asignaturas.put(clave, new Asignatura(nombre, area, semestre, clave));
-
                     }
                     break;
                 }
+            
                 case 4 -> {
                     if(profesores.size() <= 0 || asignaturas.size() <= 0){
                         System.out.println("No hay profesores registrados o asignaturas registradas");
@@ -131,9 +133,7 @@ public class ProyectoColeccionesEnJava {
                             opAux2 = sc.nextInt();
                             System.out.println("Ingrese la clave del grupo");
                             clave = sc.nextInt();
-
                             String claveGrupo = asignaturas.get(asignaturasKeys[opAux - 1]).getNombre() + Integer.toString(clave);
-
                             if(grupos.containsKey(claveGrupo)){
                                 System.out.println("Ya existe este grupo");
                             }else{
@@ -148,11 +148,11 @@ public class ProyectoColeccionesEnJava {
                         }
                     }
                     break;
-                }
-                default ->{ System.out.println("Opción no válida"); break;}
+                }*/
+                
                     
-            }
-        }while(op != 99);
+           
+        }while(selector != 99);
     }
     
 }
